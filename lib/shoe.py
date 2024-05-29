3bf6c0e5c78612e4d42431eac7a0288bef802136
gatere <mercynjerigatere@gmail.com>
2024-05-29 16:29:13 +0300

    Adds lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__ (self, brand, size):
+        self.brand = brand
+        if (isinstance(size, int)):
+            self.size = size
+        else :
+            print ("size must be an integer")
+    
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+converse = Shoe("converse", 45)
